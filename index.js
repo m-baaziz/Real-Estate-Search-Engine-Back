3a127c164b3184e762f635b55a0630568f3d41f2
m-baaziz <mha.baaziz@gmail.com>
2018-03-18 00:41:14 +0100

    fake img array added to returned housing list

--- index.js
+++ index.js
@@ -53,7 +53,12 @@ app.get('/housing', (req, res) => {
 				console.log("zipcode : ", hit._source.zipcode);
 				return Object.assign({}, hit._source, {
 					id: hit.id,
-					position: geoLocs[hit._source.zipcode]
+					position: geoLocs[hit._source.zipcode],
+					img: [
+						'https://preview.ibb.co/dxjxCx/photo1.jpg',
+						'https://preview.ibb.co/dAmNec/bedroom1.jpg',
+						'https://preview.ibb.co/nBf4sx/kitchen1.jpg'
+					]
 				});
 			});
 			res.json({ data });
