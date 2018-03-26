a0b8bce898de710001cbf7bc6929396660abd0a3
m-baaziz <mha.baaziz@gmail.com>
2018-03-27 01:45:45 +0200

    classic filters es connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const serverless = require('serverless-http');
 const elasticsearch = require('elasticsearch');
 const request = require('request');
+const _ = require('lodash');
 
 const config = require('./config');
 
@@ -37,14 +38,51 @@ app.get('/housing/scroll', (req, res) => {
 });
 
 app.get('/housing', (req, res) => {
-	const { query } = req.query;
-
+	const { city, zipcode, price, superficy, rooms, custom } = req.query;
+	const priceMin = parseInt(price.min);
+	const priceMax = parseInt(price.max);
+	const superficyMin = parseInt(superficy.min);
+	const superficyMax = parseInt(superficy.max);
+	const roomsMin = parseInt(rooms.min);
+	const roomsMax = parseInt(rooms.max);
 	// use Redis cache to destroy previous scroll memory
 
-	if (typeof query !== 'string' || query.length > DESCRIPTION_MAX_LENGTH) {
+	if (
+		(priceMin !== NaN && priceMax !== NaN && priceMin > priceMax) ||
+		(superficyMin !== NaN && superficyMax !== NaN && superficyMin > superficyMax) ||
+		(roomsMin !== NaN && roomsMax !== NaN && roomsMin > roomsMax) ||
+		(typeof zipcode !== 'string' || (zipcode.lenght > 0 && !zipcode.match(/^[0-9]{5}$/))) ||
+		(typeof custom !== 'string' || custom.length > DESCRIPTION_MAX_LENGTH) ||
+		(typeof city !== 'string' || city.length > DESCRIPTION_MAX_LENGTH)
+	) {
 		res.status(400).send('Invalid query');
 		return;
 	}
+
+	const matches = {};
+	const ranges = {price: {}, square: {}, rooms: {}};
+
+	if (city.length > 0)
+		matches.city = city;
+	if (zipcode.length > 0)
+		matches.zipcode = zipcode;
+	if (custom.length > 0)
+		matches.description = custom;
+
+	if (priceMin || priceMin === 0)
+		ranges.price.gte = priceMin;
+	if (priceMax || priceMax === 0)
+		ranges.price.lte = priceMax;
+
+	if (superficyMin || superficyMin === 0)
+		ranges.square.gte = superficyMin;
+	if (superficyMax || superficyMax === 0)
+		ranges.square.lte = superficyMax;
+
+	if (roomsMin || roomsMin === 0)
+		ranges.rooms.gte = roomsMin;
+	if (roomsMax || roomsMax === 0)
+		ranges.rooms.lte = roomsMax;
 
 	// simulate query to aproximate results lenght / refuse if query is not accurate enough
 
@@ -52,8 +90,12 @@ app.get('/housing', (req, res) => {
 		scroll: SCROLL_TIMEOUT,
 		body: {
 			query: {
-				match: {
-					description: query
+				bool: {
+					must: _.map(matches, (value, key) => {
+						return { match: { [key]: value } };
+					}).concat(_.map(ranges, (value, key) => {
+						return { range: { [key]: value } };
+					}))
 				}
 			}
 		}
