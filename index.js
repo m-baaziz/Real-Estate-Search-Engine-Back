ca89566e18545027d8d2323cae77f6fc37b25817
m-baaziz <mha.baaziz@gmail.com>
2018-04-03 02:35:18 +0200

    filter by coordinates

--- index.js
+++ index.js
@@ -37,19 +37,25 @@ app.get('/housing/scroll', (req, res) => {
 });
 
 app.get('/housing', (req, res) => {
-	const { city, zipcode, price, superficy, rooms, custom } = req.query;
+	const { city, zipcode, price, superficy, rooms, custom, lat, lon } = req.query;
 	const priceMin = parseInt(price.min);
 	const priceMax = parseInt(price.max);
 	const superficyMin = parseInt(superficy.min);
 	const superficyMax = parseInt(superficy.max);
 	const roomsMin = parseInt(rooms.min);
 	const roomsMax = parseInt(rooms.max);
+	const latMin = parseFloat(lat.min);
+	const latMax = parseFloat(lat.max);
+	const lonMin = parseFloat(lon.min);
+	const lonMax = parseFloat(lon.max);
 	// use Redis cache to destroy previous scroll memory
 
 	if (
 		(priceMin !== NaN && priceMax !== NaN && priceMin > priceMax) ||
 		(superficyMin !== NaN && superficyMax !== NaN && superficyMin > superficyMax) ||
 		(roomsMin !== NaN && roomsMax !== NaN && roomsMin > roomsMax) ||
+		(latMin !== NaN && latMax !== NaN && latMin > latMax) ||
+		(lonMin !== NaN && lonMax !== NaN && lonMin > lonMax) ||
 		(typeof zipcode !== 'string' || (zipcode.lenght > 0 && !zipcode.match(/^[0-9]{5}$/))) ||
 		(typeof custom !== 'string' || custom.length > DESCRIPTION_MAX_LENGTH) ||
 		(typeof city !== 'string' || city.length > DESCRIPTION_MAX_LENGTH)
@@ -59,7 +65,7 @@ app.get('/housing', (req, res) => {
 	}
 
 	const matches = {};
-	const ranges = {price: {}, square: {}, rooms: {}};
+	const ranges = { price: {}, square: {}, rooms: {}, 'coordinates.lat': {}, 'coordinates.lon': {} };
 
 	if (city.length > 0)
 		matches.city = city;
@@ -82,6 +88,16 @@ app.get('/housing', (req, res) => {
 		ranges.rooms.gte = roomsMin;
 	if (roomsMax || roomsMax === 0)
 		ranges.rooms.lte = roomsMax;
+
+	if (latMin || latMin === 0)
+		ranges['coordinates.lat'].gte = latMin;
+	if (latMax || latMax === 0)
+		ranges['coordinates.lat'].lte = latMax;
+
+	if (lonMin || lonMin === 0)
+		ranges['coordinates.lon'].gte = lonMin;
+	if (lonMax || lonMax === 0)
+		ranges['coordinates.lon'].lte = lonMax;
 
 	// simulate query to aproximate results lenght / refuse if query is not accurate enough
 
